9a7316c11897c8edfc6fa170b2731c845be4d550
MAC <mac@MACs-MacBook-Air.local>
2021-12-22 13:12:18 +0300

    done

--- calculator.js
+++ calculator.js
@@ -2,5 +2,23 @@ const prompt = require("prompt-sync")(); // Please don't touch me :)
 
 // The prompt method allows us to take an input from the user.
 // Run the code to see how to use prompt.
-const name = prompt("What's your name? ");
-console.log(`Hi, I'm ${name}.`);
+const number1 = prompt("type the first number");
+const number2 = prompt("type the second number");
+const operation = prompt("type the operation:+,-,*,/");
+
+if (operation == "+") {
+  let sum = +number1 + +number2;
+  console.log(number1, " ", operation, " ", number2, "=", sum);
+}
+if (operation == "/") {
+  let sum = +number1 / +number2;
+  console.log(number1, " ", operation, " ", number2, "=", sum);
+}
+if (operation == "*") {
+  let sum = +number1 * +number2;
+  console.log(number1, " ", operation, " ", number2, "=", sum);
+}
+if (operation == "-") {
+  let sum = +number1 - +number2;
+  console.log(number1, " ", operation, " ", number2, "=", sum);
+}
